c482f1d8e3c61f6c4bd06bb4c1b472bf2517e769
Matthew Slater <slaterm100@gmail.com>
2021-06-26 18:56:49 -0400

    complete laser function for Space invader

---
+++ js/script.js
@@ -0,0 +1,130 @@
+const grid = document.querySelector('.grid')
+let currentShooterIndex = 202
+let width = 15
+let direction = 1
+let speed = 100
+let invadersId = null
+let goingRight = true
+const resultsDisplay = document.querySelector('.results')
+
+for (let i = 0; i < 225; i++) {
+    const square = document.createElement('div')
+    grid.appendChild(square)
+}
+
+const squares = Array.from(document.querySelectorAll('.grid div'))
+
+
+const alienInvaders = [
+    0,1,2,3,4,5,6,7,8,9,
+    15,16,17,18,19,20,21,22,23,24,
+    30,31,32,33,34,35,36,37,38,39
+]
+
+function draw() {
+    for (let i = 0; i < alienInvaders.length; i++) {
+        squares[alienInvaders[i]].classList.add('invader')
+    }
+}
+
+//add pictures for the aliens 
+
+draw()
+
+function remove() {
+    for (let i = 0; i < alienInvaders.length; i++) {
+        squares[alienInvaders[i]].classList.remove('invader')
+    }
+}
+
+
+squares[currentShooterIndex].classList.add('shooter')
+
+function moveShooter(e) {
+    squares[currentShooterIndex].classList.remove('shooter')
+    switch(e.key) {
+        case 'ArrowLeft' :
+            if (currentShooterIndex % width !== 0) currentShooterIndex -=1
+            break
+        case 'ArrowRight' : 
+            if (currentShooterIndex % width < width -1) currentShooterIndex +=1
+            break
+    }
+    squares[currentShooterIndex].classList.add('shooter')    
+}
+
+document.addEventListener('keydown', moveShooter)
+
+function moveInvaders() {
+    const leftEdge = alienInvaders[0] % width === 0
+    const rightEdge = alienInvaders[alienInvaders.length - 1] % width === width -1
+    remove()
+
+    if (rightEdge && goingRight ) {
+        for (let i = 0; i < alienInvaders.length; i++){
+            alienInvaders[i] += width +1
+            direction = -1
+            goingRight = false
+        }
+    }
+    if (leftEdge && !goingRight) {
+        for (let i = 0; i < alienInvaders.length; i++){
+            alienInvaders[i] += width -1
+            direction = 1
+            goingRight = true 
+        }
+    }
+
+    for (let i = 0; i < alienInvaders.length; i++) {
+        alienInvaders[i] += direction 
+    }
+
+    draw()
+// confirm if this is necessary 
+//set variable speed for levels 
+//win round function 
+
+    if (squares[currentShooterIndex].classList.contains('invader','shooter')) {
+        alert("GAME OVER")
+        resultsDisplay.innerHTML = 'GAME OVER'
+        clearInterval(invadersId)
+    }
+
+    for (let i = 0; i < alienInvaders.length; i++) {
+        if (alienInvaders[i] > squares.length){
+        alert("GAME OVER")
+        resultsDisplay.innerHTML = 'GAME OVER'
+        clearInterval(invadersId)
+        }
+    }
+}
+
+invadersId = setInterval(moveInvaders, speed)
+
+function shoot(e) {
+    let laserId
+    let currentLaserIndex = currentShooterIndex
+    function moveLaser() {
+        squares[currentLaserIndex].classList.remove('laser')
+        currentLaserIndex -= width
+        squares[currentLaserIndex].classList.add('laser')
+
+        if (squares[currentLaserIndex].classList.contains('invader')) {
+            squares[currentLaserIndex].classList.remove('laser')
+            squares[currentLaserIndex].classList.remove('invader')
+            squares[currentLaserIndex].classList.add('boom')
+            //add sound to these events 
+          
+            setTimeout(()=> squares[currentLaserIndex].classList.remove('boom'),300)
+            clearInterval(laserId)
+        }
+
+    }
+    switch(e.key) {
+        //using space bar here
+        case ' ': 
+        laserId = setInterval(moveLaser, 75)
+    }
+}
+
+document.addEventListener('keydown',shoot)
---
+++ readme.md
@@ -0,0 +1,4 @@
+# Project #1: The Game
+
+1. Simplified space invaders in JS
+2. Draft 1 
